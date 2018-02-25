79c653894212badbc4bf29cf8b0227c12be6241d
LeoClassic <pakarat.mus@gmail.com>
2018-02-25 21:15:54 +0700

    Add files via upload

---
+++ Metaheuristic_TSFuzzy.m
@@ -0,0 +1,72 @@
+function [] = Metaheuristic_TSFuzzy
+data = importdata('BoxJenkins.mat');
+data = [data(3:end,2),data(2:end-1,2),data(1:end-2,2),data(3:end,1),data(2:end-1,1)];
+X = data(:,2:end);
+T = data(:,1);
+
+xTrain = X(1:size(X,1)/2,:);
+tTrain = T(1:size(X,1)/2,:);
+xTest = X(size(X,1)/2+1:end,:);
+tTest = T(size(X,1)/2+1:end,:);
+
+nInput = size(xTrain,2);
+
+upper_input = max(X);
+lower_input = min(X);
+upper_sigma = 50*ones(1,nInput);
+lower_sigma = 1e-5*ones(1,nInput);
+upper_weight = 6*ones(1,nInput);
+lower_weight = -6*ones(1,nInput);
+upper_bias = 6;
+lower_bias = -6;
+
+nRule = 3;
+
+UB = repmat([upper_input, upper_sigma, upper_weight, upper_bias],1,nRule);
+LB = repmat([lower_input, lower_sigma, lower_weight, lower_bias],1,nRule);
+
+dim = nRule*(nInput*3+1);
+
+
+objFun = @(solution)TSFuzzy_fitness(solution,nRule,xTrain,tTrain);
+
+solution = PSO(objFun,100,20000,dim,LB,UB);
+
+
+model = constructor(solution,nInput,nRule);
+output = estimator(xTest,model);
+
+subplot(2,1,1)
+hold on
+plot(tTest,'b')
+plot(output,'r')
+subplot(2,1,2)
+plot(tTest-output,'b')
+end
+
+function model = constructor(solution,nInput,nRule)
+solution = reshape(solution,length(solution)/nRule,nRule)';
+model.center = solution(:,1:nInput);
+model.sigma = solution(:,nInput+1:nInput*2);
+model.weight = solution(:,nInput*2+1:end-1);
+model.bias = solution(:,end);
+model.nRule = nRule;
+end
+
+function output = estimator(X,model)
+Y = inf(size(X,1),1);
+for i = 1:size(X,1)
+    h = -((repmat(X(i,:),model.nRule,1)-model.center)./model.sigma).^2;
+    h = exp(sum(h,2));
+    mu = h./sum(h);
+    Y(i) = (model.weight*X(i,:)'+model.bias)'*mu;
+end
+output = Y;
+end
+
+function fitness = TSFuzzy_fitness(solution,nRule,X,T)
+model = constructor(solution,size(X,2),nRule);
+Y = estimator(X,model);
+fitness = (Y-T)'*(Y-T);
+end
+
---
+++ PSO.m
@@ -0,0 +1,89 @@
+function solution = PSO(fobj,nPop,nFEs,dim,LB,UB)
+%% Parameters
+NP = nPop;
+MaxIter = round(nFEs/nPop);
+c1 = 2;
+c2 = 2;
+
+Dim = dim;
+
+wMax = 0.9;         %Max inirtia weight
+wMin = 0.5;         %Min inirtia weight
+
+%% Defined lower bound and upper bound.
+LB = repmat(LB,NP,1);
+UB = repmat(UB,NP,1);
+
+%% Initialize swarm population randomly
+population =  LB+(UB-LB).*rand(NP,Dim);
+ibest = Inf;
+
+%% Randomly initialise velocity
+vmax = ones(NP,Dim).*(UB-LB)./10;
+velocity=vmax.*rand(NP,Dim);
+
+%% Evaluate initial population
+fvalue = Inf(NP,1);
+fbestval = Inf;
+nfe = 0;
+for i = 1:NP,
+    fvalue(i) = fobj(population(i,:));
+    nfe = nfe+1;
+    % Finding best particle in initial population
+    if fvalue(i) <= fbestval
+        fbestval = fvalue(i);
+        ibest = i;
+    end
+end
+
+gbest = repmat(population(ibest,:),[NP 1]);
+bestPara = population(ibest,:);
+pbest = population; % Initializing Best positions matrix
+fpbest = fvalue;    % Initializing the corresponding function values
+
+%% Main loop
+iteration = 0;
+while nfe < nFEs
+    
+    R1 = rand(NP,Dim);
+    R2 = rand(NP,Dim);
+    
+    iteration = iteration+1;
+    w = wMin-iteration*(wMax-wMin)/MaxIter;
+    
+    velocity = w*(velocity + c1*R1.*(pbest-population) + c2*R2.*(gbest-population));
+    velocity=(velocity<-vmax).*(-vmax)+(velocity>vmax).*(vmax)+(velocity>-vmax&velocity<vmax).*velocity;
+    
+    %% Update the swarm particle
+    population = population + velocity;
+    
+    population(population>UB)=UB(population>UB);
+    population(population<LB)=LB(population<LB);
+    
+    %% Evaluate the new swarm
+    for i = 1:NP,
+        fvalue(i) = fobj(population(i,:));
+        nfe = nfe+1;
+    end
+    
+    %% Updating the pbest for each particle
+    changeRows = fvalue < fpbest;
+    pbest(changeRows,:) = population(changeRows,:);
+    fpbest = fpbest.*~changeRows + fvalue.*changeRows;
+    
+    %% Updating best particle gbest
+    [fbestval_, ibest] = min(fpbest);
+    %gbest = repmat(population(ibest,:),[NP 1]);
+    %bestPara = population(ibest,:);
+    if fbestval_ < fbestval
+        fbestval = fbestval_;
+        gbest = repmat(population(ibest,:),[NP 1]);
+        bestPara = population(ibest,:);
+    end
+    
+    if mod(iteration,10)==0
+        fprintf('iteration: %d, fBest: %f \n',iteration,fbestval);
+    end
+end
+solution = bestPara;
+end
